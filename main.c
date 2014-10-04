04377b69b396a3b3ebf74aa2a5feb444044596eb
sukumargaonkar <sukumargaonkar@gmail.com>
2014-10-04 22:05:19 +0530

    Error fixed: ball going out of arena

--- main.c
+++ main.c
@@ -21,23 +21,23 @@ typedef struct
 
 void const_ball(float x,float y,float a,float b,ball *e);
 
-int i,j,window_width=500,window_height=500;
+int i,j,window_width=500,window_height=500,player_life = 3;
 int ball_no=4;
-float speed=0.02,player_speed=0.05;
+float enemy_speed=0.07,player_speed=0.05;
 ball enemy[4], player;
 float cursor_pos_x=0,cursor_pos_y=0;
 
 
 //float x1=-1.0,x2=2.0,y11=0.0,y2=-2.0;
 //float x1_i=0.015,x2_i=-0.015,x1_j=0.000,x2_j=0.015;
-static int flag=1;
+static int flag=0;
 
 void const_ball(float x,float y,float a,float b,ball *obj)
 {
     obj->pos_x=x;
     obj->pos_y=y;
-    obj->vel_x=a * speed;
-    obj->vel_y=b * speed;
+    obj->vel_x=a * enemy_speed;
+    obj->vel_y=b * enemy_speed;
 
 }
 
@@ -65,8 +65,11 @@ void keyPressed(int key,int x,int y)
 }
 
 void mouse_click(int button, int state, int x, int y) {
-if(state == GLUT_DOWN && button == GLUT_LEFT_BUTTON)
-    printf("Left click\n");
+    if(state == GLUT_DOWN && button == GLUT_LEFT_BUTTON)
+    {
+         printf("Left click\n");
+         flag = 1;
+    }
 }
 
 void mouse_co_ordinates(int x,int y)
@@ -80,7 +83,7 @@ void mouse_co_ordinates(int x,int y)
 
 //        player.vel_x = (cursor_pos_x - player.pos_x) * player_speed;
 //        player.vel_y = (cursor_pos_y - player.pos_y) * player_speed;
-printf("diff: %f  vel: %f\n",(cursor_pos_x - player.pos_x),player.vel_x);
+//printf("diff: %f  vel: %f\n",(cursor_pos_x - player.pos_x),player.vel_x);
 //
 //        player.vel_x = cursor_pos_x - player.pos_x;
 //        player.vel_y = cursor_pos_y - player.pos_y;
@@ -105,6 +108,26 @@ void drawBall(ball *obj)
 
 }
 
+void enemy_player_coll(ball *enemy)
+{/*
+    float val = sqrt((player.pos_x - enemy->pos_x)*(player.pos_x - enemy->pos_x) + (player.pos_y - enemy->pos_y)*(player.pos_y - enemy->pos_y));
+    if(val < 0.4)
+    {
+        player_life--;
+        if(player_life == 0);
+            //flag = 0;
+        float temp = player.vel_x;
+        player.vel_x = enemy->vel_x;
+        enemy->vel_x = temp;
+
+        temp = player.vel_y;
+        player.vel_y = enemy->vel_y;
+        enemy->vel_y = temp;
+
+    }
+    */
+}
+
 
 void ball_ball_coll(ball *obj1,ball *obj2)
 {
@@ -123,6 +146,12 @@ void ball_ball_coll(ball *obj1,ball *obj2)
         temp = obj1->vel_y;
         obj1->vel_y = obj2->vel_y;
         obj2->vel_y = temp;
+
+        obj1->pos_x += obj1->vel_x;
+        obj1->pos_y += obj1->vel_y;
+        obj2->pos_x += obj2->vel_x;
+        obj2->pos_y += obj2->vel_y;
+
         //printf("%f %f %f %f %f %f\n",x1,x2,x1_i,x1_j,x2_i,x2_j);
         //flag = 0;
 
@@ -133,14 +162,27 @@ void ball_ball_coll(ball *obj1,ball *obj2)
 
 void ball_wall_coll(ball *obj)
 {
-    if((obj->pos_x + 0.4) >= 2 || (obj->pos_x - 0.4) <= -2)
+    if((obj->pos_x + 0.2) >= 2)
     {
         obj->vel_x = obj->vel_x * -1;
-                //printf("coll x");
+        obj->pos_x = 1.8;
     }
-    if((obj->pos_y + 0.4) >= 2 || (obj->pos_y - 0.4) <= -2)
+    else if((obj->pos_x - 0.2) <= -2)
+    {
+        //printf("coll x %f\n",obj->vel_x);
+        obj->vel_x = obj->vel_x * -1;
+        obj->pos_x = -1.8;
+
+    }
+    if((obj->pos_y + 0.2) >= 2)
     {
         obj->vel_y = obj->vel_y * -1;
+        obj->pos_y = 1.8;
+    }
+    else if((obj->pos_y - 0.2) <= -2)
+    {
+        obj->vel_y = obj->vel_y * -1;
+        obj->pos_y = -1.8;
         //printf("coll y");
     }
 
@@ -154,10 +196,10 @@ void update()
     {
         for(i=0;i<ball_no;i++)
         {
-                // Detect Ball - Wall collision
+            // Detect Ball - Wall collision
             ball_wall_coll(&enemy[i]);
 
-                // Detect Ball - Ball collision
+            // Detect Ball - Ball collision
             for(j=i+1;j<ball_no;j++)
             {
                 if(i == 3)
@@ -168,14 +210,24 @@ void update()
 
             enemy[i].pos_x += enemy[i].vel_x;
             enemy[i].pos_y += enemy[i].vel_y;
+            if(i == 2)
+                printf("%d: %f %f  %f %f\n",i,enemy[i].pos_x,enemy[i].pos_y,enemy[i].vel_y,enemy[i].vel_y);
         }
 
+
+
     }
+
+    for(i=0;i<ball_no;i++)
+    {
+         enemy_player_coll(&enemy[i]);
+    }
+
 
     float temp_x = (cursor_pos_x - player.pos_x);
     float temp_y = (cursor_pos_y - player.pos_y);
     float temp_xy = sqrt((temp_x * temp_x) + (temp_y * temp_y));
-    printf("temp: %f %f %f\n",temp_x,temp_y,temp_xy);
+    //printf("temp: %f %f %f\n",temp_x,temp_y,temp_xy);
     if(temp_xy != 0)
     {
         player.vel_x = temp_x / temp_xy * player_speed;
@@ -208,19 +260,16 @@ void display()
      update();
 
     // Draw Enemies
-    /*
+
     for(i=0;i<ball_no;i++)
     {
         //printf("%d\n",i);
          drawBall(&enemy[i]);
     }
-    */
+
 
     // Draw player
     drawBall(&player);
-
-
-
 
 
     glutSwapBuffers();
@@ -243,13 +292,13 @@ int main(int argc,char **argv)
 
     //Init
     // instantiate Enemies
-    /*
-    const_ball(1.0,1.0,-.857,-0.514,&enemy[0]);
+
+    const_ball(1.0,1.0,-0.857,-0.514,&enemy[0]);
     const_ball(-1.5,0.2,0.514,-0.857,&enemy[1]);
     const_ball(0.6,-1.0,-0.919,0.394,&enemy[2]);
     const_ball(-0.4,-1.4,0.965,0.263,&enemy[3]);
 
-    */
+
     // Instantiate player
     const_ball(0,0,0,0,&player);
 //printf("Creating object %f %f\n",player.vel_x,player.vel_y);
@@ -259,7 +308,7 @@ int main(int argc,char **argv)
 
     glutReshapeFunc(reshape);
 
-    //glutMouseFunc(mouse_click);
+    glutMouseFunc(mouse_click);
     glutPassiveMotionFunc(mouse_co_ordinates);
 
     glutSpecialFunc(keyPressed);
